76614cf5d945cb98bc5ace48d614270e8271912f
GG-Henry1833 <69804977+GG-Henry1833@users.noreply.github.com>
2020-11-19 10:09:39 +0800

    Update FlashSploit.sh

--- FlashSploit.sh
+++ FlashSploit.sh
@@ -1,6 +1,9 @@
 #data/data/com.termux/files/usr/bin/bash
 #create by HENRY1833
 
+import os from system
+import smtplib
+
 def main():
    print'======================================='
    print'          Create By HENRY1833          '
