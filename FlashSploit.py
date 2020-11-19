b7a6fbe73c4b95743caa7e7770160cc3b42ac0e1
GG-Henry1833 <69804977+GG-Henry1833@users.noreply.github.com>
2020-11-19 09:44:52 +0800

    FlashSploit.py
    Hi

---
+++ FlashSploit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+'''create by Henry1833'''
+
+import os from system
+
+def main()
+   print'======================================='
+   print'          Create By HENRY1833          '
+   print'======================================='
+   
